17f8d4336d3a459ba29b3d2f850a07b8039ca638
shobhit1813 <shobhit.s1813@gmail.com>
2018-12-14 01:57:12 +0530

    initial commit

--- src/java/Controller/Crawler.java
+++ src/java/Controller/Crawler.java
@@ -1,44 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controller;
+import java.util.Scanner;
 
-import Modal.CrawlerDB;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-/**
- *
- * @author shobhit
- */
-public class Crawler {
-    
-    public static CrawlerDB db = new CrawlerDB();
-    
-    public static void main(String args[])throws IOException,SQLException{
-        
-        db.runSql2("Truncate Record");
-        processPage("index.html");
-    }
-    public static void processPage(String url)throws SQLException,IOException{
-       
-        String sql = "select * from spidy where URL ='"+url+"'";
-        ResultSet rs = db.runSql(sql);
-        if(rs.next()){
-            
+public class Crawler{
+
+    int file_no = 0;
+    ValidateDetails valid_check;
+    Doc my_doc;
+    Hashable hash;
+    hash[] my_hash;
+
+    public Crawler(){
+        valid_check = new Validate();
+        my_doc = new Doc();
+        hash = new Hashable();
+        my_hash = new hash[100];
+
+        for(int i = 0; i < my_hash.length; i++){
+            my_hash[i] = new hash();
         }
-        else{
-            
-        }
-        
     }
-    
+    public void executeCrawler(String url,String dir, int depth){
+        int flag = 0;
+        flag = valid_check.checkURL(url) + valid_check.checkDirectory(dir) + valid_check.checkDepth(depth);
+
+        if(flag == 3){
+            String html = new String();
+            html = my_doc.getIn(url);
+        }
+    }
+    public static void main(String args[]){
+        
+        Crawler cw = new Crawler();
+        Scanner sc = new Scanner(System.in);
+        cw.executeCrawler("index.html/","home/Desktop/NetBeansProjects/WebSpider/web",2);
+    }
 }
