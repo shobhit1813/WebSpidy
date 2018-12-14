a71e5271f028ad049cc703d8a33bb13021aff8f6
shobhit1813 <shobhit.s1813@gmail.com>
2018-12-14 10:29:18 +0530

    itermediate commit

--- src/java/Controller/Crawler.java
+++ src/java/Controller/Crawler.java
@@ -5,13 +5,13 @@ public class Crawler{
     int file_no = 0;
     ValidateDetails valid_check;
     Doc my_doc;
-    Hashable hash;
+    Hashable h;
     hash[] my_hash;
 
     public Crawler(){
         valid_check = new Validate();
         my_doc = new Doc();
-        hash = new Hashable();
+        h = new Hashable();
         my_hash = new hash[100];
 
         for(int i = 0; i < my_hash.length; i++){
@@ -23,14 +23,28 @@ public class Crawler{
         flag = valid_check.checkURL(url) + valid_check.checkDirectory(dir) + valid_check.checkDepth(depth);
 
         if(flag == 3){
-            String html = new String();
-            html = my_doc.getIn(url);
+           do{ 
+                String html = new String();
+                html = my_doc.parseHtml(url);
+                file_no = my_doc.writeIntoFile(html, dir,file_no);
+                my_hash = h.set(my_hash, my_doc.getInArray(html, url));
+                my_hash[0].print();
+
+                while(flag != 1){
+                    url = new String(hash.getNextURL(my_hash,depth));
+                    System.out.println(url);
+                    if(url.equals("-1"))
+                        return;
+                    flag = valid_check.checkURL(url);
+                }
+                flag = 0;
+            }while(file_no <1000);
         }
     }
     public static void main(String args[]){
         
         Crawler cw = new Crawler();
         Scanner sc = new Scanner(System.in);
-        cw.executeCrawler("index.html/","home/Desktop/NetBeansProjects/WebSpider/web",2);
+        cw.executeCrawler("https://www.chitkarauniversity.edu.in","home/Desktop/shobhit",2);
     }
 }
---
+++ src/java/Controller/ValidateDetails.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+
+class ValidateDetails{
+
+    //function to check whether url is valid(eg. )
+    public int checkURL(String url){
+        try{
+            URL objurl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection)objurl.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            String url_type = new String(getContentType());
+            System.out.prinltn("Content Type of "+url+": "+url_type);
+            if(url_type == "text/html")
+                return 1;
+            
+        }
+        catch(MalformedURLException e){
+            System.out.prinltn(e.toString());
+        }
+        catch(ProtocolException e){
+            System.out.println(e.toString());
+        }
+        catch(IOException e){
+            System.out.println(e.toString());
+        }
+        return 0;
+    }
+    public int checkDepth(int depth){
+
+        if(depth >= 0)
+            return 1;
+        return 0;
+    }
+}
